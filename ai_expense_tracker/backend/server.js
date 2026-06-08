613f7ae2091fcaaa22b75ad641dcc912f3238932
Satvik Mittal <knowsatviks@gmail.com>
2026-06-08 17:59:00 +0530

    Added controllers and integrated gemini for insights on transactions and budgets

--- ai_expense_tracker/backend/server.js
+++ ai_expense_tracker/backend/server.js
@@ -4,6 +4,11 @@ import dotenv from "dotenv";
 import { Pool } from "pg";
 import jwt from "jsonwebtoken";
 import authRoutes from "./routes/authRoutes.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
+import transactionRoutes from "./routes/transactionRoutes.js";
+import budgetRoutes from "./routes/budgetRoutes.js";
+import dashboardRoutes from "./routes/dashboardRoutes.js";
+import insightsRoutes from "./routes/insightsRoutes.js";
 
 dotenv.config(); //it makes sure that process.env contains the variables defined in the .env file
 
@@ -20,6 +25,12 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/auth", authRoutes);
+app.use("/api/categories", categoryRoutes);
+app.use("/api/transactions", transactionRoutes);
+app.use("/api/budgets", budgetRoutes);
+app.use("/api/dashboard", dashboardRoutes);
+app.use("/api/insights", insightsRoutes);
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
